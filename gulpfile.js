439d3521064e76ffa7c62e635bf5a48c5bc0297a
mia <m.09048030369@gmail.com>
2021-08-17 16:06:44 +1000

    init

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+// Initialize modules
+const { src, dest, watch, series, parallel } = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const replace = require('gulp-replace');
+const sass = require('gulp-sass')(require('sass'));
+const fibers = require('fibers');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+sass.compiler = require('sass');
+
+// File pass variables
+const files = {
+  scssPath: 'src/scss/**/*.scss', // any file with .scss
+  jsPath: 'src/js/**/*.js', // any file with .js
+};
+// Sass task to compile sass files
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass(
+      { fiber: fibers }
+    ))
+    .pipe(
+      plumber({
+        errorHandler: notify.onError({
+          title: 'Error...',
+          message: '<%= error.message %>',
+        }),
+      })
+    )
+    .pipe(sass({ outputStyle: 'compressed', errLogToConsole: false }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist'));
+}
+// JS task minify JS file
+function jsTask() {
+  return src(files.jsPath)
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(dest('dist'));
+}
+
+// Cachebusting task
+const cbString = new Date().getTime();
+function cacheBustingTask() {
+  return src(['index.html'])
+    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+    .pipe(dest('.'));
+}
+// Watch task
+function watchTask() {
+  watch([files.scssPath, files.jsPath], parallel(scssTask, jsTask));
+}
+
+// Default task
+exports.default = series(
+  parallel(scssTask, jsTask),
+  cacheBustingTask,
+  watchTask
+);
