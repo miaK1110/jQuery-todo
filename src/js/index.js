6a0dfcacf1beb776599faf5efe129c09acd7affa
mia <m.09048030369@gmail.com>
2021-08-19 17:12:01 +1000

    4th commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,10 @@ $('.js-add-todo').on('click', function (e) {
   e.preventDefault();
 
   const addText = $('.js-get-val').val();
-  $('js-get-val').val('');
+  $('.js-get-val').val('');
 
   if (!addText) {
-    $('js-toggle-error').show();
-    console.log('put something');
+    $('.js-toggle-error').show();
     return;
   }
 
@@ -16,8 +15,7 @@ $('.js-add-todo').on('click', function (e) {
     '<li class="p-listItem js-todo_list-item" data-text="' +
     addText +
     '">' +
-    '<i class="c-icon p-listItem__iconTick far fa-check-circle js-click-done" aria-hidden="true">' +
-    addText +
+    '<i class="c-icon p-listItem__iconTick far fa-check-circle js-click-done" aria-hidden="true"></i>' +
     '</i>' +
     '<span class="p-listItem__text js-todo_list-text">' +
     addText +
@@ -25,20 +23,12 @@ $('.js-add-todo').on('click', function (e) {
     '<input type="text" class="p-listItem__editText js-todo_list-editForm" value="' +
     addText +
     '">' +
-    '<i class="c-icon p-listItem__iconBin fa fa-trash icon-trash js-click-trash" aria-hidden="true"></i>' +
+    '<i class="c-icon p-listItem__iconBin fa fa-trash icon-trash js-click-delete" aria-hidden="true"></i>' +
     '</li>';
-
   $('.js-todo_list').prepend(listItem);
 });
 
-//【2. TODOタスクのアイコンを押下した際にタスクをDONEにする】
-// 1. TODOタスクのアイコンを押下した際にイベントを発火
-// 2. クリックしたDOM（アイコン）をdoneのアイコンに変更
-// 3. クリックしたDOM（アイコン）にjs-click-doneのクラス名を削除し、js-click-todoのクラス名をつける
-// 4. クリックしたDOM（アイコン）から辿って、list__itemのDOMを取得
-// 5. list__itemのクラス名をdoneのものに変更する
-
-// toggle to done the task
+// toggle to set todo to done
 
 $(document).on('click', '.js-click-done', function () {
   $(this)
@@ -50,7 +40,7 @@ $(document).on('click', '.js-click-done', function () {
     .addClass('p-listItem--done');
 });
 
-// toggle to undo done task
+// toggle to set todo to undone
 
 $(document).on('click', '.js-click-undo', function () {
   $(this)
@@ -63,7 +53,7 @@ $(document).on('click', '.js-click-undo', function () {
   console.log('aaaa');
 });
 
-// delete task
+// delete todo
 
 $(document).on('click', '.js-click-delete', function () {
   $(this)
@@ -73,26 +63,52 @@ $(document).on('click', '.js-click-delete', function () {
     });
 });
 
-// edit task
+// edit todo
+
 $(document).on('click', '.js-todo_list-text', function () {
-  $(this).hide().siblings('.js-todo_list-editForm').show();
-  if ($('li').hasClass('p-listItem--done')) {
-    alert(this);
-    $(this).addClass('p-listItem__editText--done');
-  }
+  $(this).hide().siblings('.js-todo_list-editForm').show().focus();
 });
 
-$(document).on('change', 'js-todo_list-editForm', function (e) {
-  console.log('aaaaaaaaaaaaaaaa');
+$(document).on('focus', '.js-todo_list-editForm', function () {
+  $(this).select();
+});
 
-  if (e.keycode == 13) {
-    var $this = $(this);
-    $this
-      .hide()
-      .siblings('.js-todo_list-text')
-      .text($this.val())
-      .show()
-      .closest('.js-todo_list-item')
-      .attr('data-text', $this.val());
+$(document).on('keyup', '.js-todo_list-editForm', function (e) {
+  if (e.which === 13) {
+    $(this).blur();
   }
+});
+$(document).on('blur', '.js-todo_list-editForm', function () {
+  const $this = $(this);
+  const todoText = $this.siblings('.js-todo_list-text').text();
+
+  if (!$this.val()) {
+    $this.val(todoText);
+  }
+
+  $this
+    .hide()
+    .siblings('.js-todo_list-text')
+    .text($this.val())
+    .show()
+    .closest('.js-todo-list_item')
+    .attr('data-text', $this.val());
+});
+
+// search todos
+
+$('.js-search').on('keyup', function () {
+  var searchText = $(this).val();
+
+  $('.js-todo_list-item')
+    .show()
+    .each(function (i, elm) {
+      const text = $(elm).data('text');
+      const regexp = new RegExp('^' + searchText);
+
+      if (text && text.match(regexp)) {
+        return true;
+      }
+      $(elm).hide();
+    });
 });
